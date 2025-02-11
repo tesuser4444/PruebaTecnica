d00be8a0598d937dd6db42639217335f683f7b33
Jorge Tirado Sandoval <52668750+tesuser4444@users.noreply.github.com>
2025-02-10 21:24:53 -0600

    feature(migration): anadi migraciontes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import { PrismaClient } from "@prisma/client";
-import login from "./controllers/loginController";
-import register from "./controllers/registerController";
-import dashboard from "./controllers/dashboardController";
+import loginController from "./controllers/loginController.js";
+import register from "./controllers/registerController.js";
+import dashboard from "./controllers/dashboardController.js";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -14,8 +14,18 @@ const SECRET_KEY = "supersecretkey";
 app.use(cors());
 app.use(express.json());
 
+// Ensure database connection
+(async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to SQLite database");
+  } catch (error) {
+    console.error("Failed to connect to the database", error);
+  }
+})();
+
+app.use("/login", loginController);
 app.use("/register", register);
-app.use("/login", login);
 app.use("/dashboard", dashboard);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
