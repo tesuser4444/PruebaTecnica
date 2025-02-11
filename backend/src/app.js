0b9eadbfe23925ecd3f9acb783d9a586cccf8df4
Jorge Tirado Sandoval <52668750+tesuser4444@users.noreply.github.com>
2025-02-10 20:49:35 -0600

    feature(app): cree configuracion inicial

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+const PORT = 5000;
+const SECRET_KEY = "supersecretkey";
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export { app, prisma, SECRET_KEY };
