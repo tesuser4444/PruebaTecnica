bab81fb379405bcd17dec13e1c02bc0af777b293
Jorge Tirado Sandoval <52668750+tesuser4444@users.noreply.github.com>
2025-02-11 21:16:06 -0600

    feature(auth): Se cambiaron los colores de texto

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="p-8 bg-white rounded shadow-md w-96">
-                <h1 className="text-2xl font-bold mb-4">Login</h1>
+                <h1 className="text-2xl font-bold mb-4 text-gray-900">Login</h1>
                 <AuthForm isLogin={true} />
             </div>
         </div>
--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -4,7 +4,7 @@ export default function RegisterPage() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="p-8 bg-white rounded shadow-md w-96">
-                <h1 className="text-2xl font-bold mb-4">Register</h1>
+                <h1 className="text-2xl font-bold mb-4 text-gray-900">Register</h1>
                 <AuthForm isLogin={false} />
             </div>
         </div>
